c536e1d923cfce3061f10f5cff07b1f06e82835c
Franticpudding <75979471+Franticpudding@users.noreply.github.com>
2020-12-14 11:10:36 +0000

    v1.0 of MOTD maker, basic system created

---
+++ MOTDmaker.py
@@ -0,0 +1,33 @@
+ #colours
+colours = {
+    "black": r"\u00A70",
+    "dark_blue": r"\u00A71",
+    "dark_green": r"\u00A72",
+    "dark_aqua": r"\u00A73",
+    "dark_red": r"\u00A74",
+    "dark_purple": r"\u00A75",
+    "gold": r"\u00A76",
+    "gray": r"\u00A77"
+}
+
+colours2 = {
+    "black"
+    "dark_blue"
+    "dark_green"
+    "dark_aqua"
+    "dark_red"
+    "dark_purple"
+    "gold"
+    "gray"
+}
+
+text=input("Please input your text: ")
+
+print("There are these many colours to choose from:",len(colours))
+print("Here are the colours: ",("\n",colours2,))
+
+ #the "dig_" are the seperate digits
+dig1=text[1-1]
+dig2=text[2-1]
+ #printing the final product with colour
+print ("\n","Here is the final MOTD code:",colours['black'])
