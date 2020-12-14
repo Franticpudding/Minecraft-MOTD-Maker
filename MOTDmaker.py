865805f61c7bf42a76cbbcfe8b8b2397a5dd3640
Franticpudding <75979471+Franticpudding@users.noreply.github.com>
2020-12-14 12:22:58 +0000

    Included full colour library and better interface

--- MOTDmaker.py
+++ MOTDmaker.py
@@ -1,33 +1,39 @@
- #colours
-colours = {
-    "black": r"\u00A70",
-    "dark_blue": r"\u00A71",
-    "dark_green": r"\u00A72",
-    "dark_aqua": r"\u00A73",
-    "dark_red": r"\u00A74",
-    "dark_purple": r"\u00A75",
-    "gold": r"\u00A76",
-    "gray": r"\u00A77"
-}
+again="n"
+while again !="y":
+    
+    colours = {
+        "black": r"\u00A70",
+        "dark blue": r"\u00A71",
+        "dark green": r"\u00A72",
+        "dark aqua": r"\u00A73",
+        "dark red": r"\u00A74",
+        "dark purple": r"\u00A75",
+        "gold": r"\u00A76",
+        "gray": r"\u00A77",
+        "dark gray": r"\u00A78",
+        "indigo": r"\u00A79",
+        "green": r"\u00A7a",
+        "aqua": r"\u00A7b",
+        "red": r"\u00A7c",
+        "pink": r"\u00A7d",
+        "yellow": r"\u00A7e",
+        "white": r"\u00A7f"
+    }
 
-colours2 = {
-    "black"
-    "dark_blue"
-    "dark_green"
-    "dark_aqua"
-    "dark_red"
-    "dark_purple"
-    "gold"
-    "gray"
-}
 
-text=input("Please input your text: ")
+    text=input("Please input your text: ")
 
-print("There are these many colours to choose from:",len(colours))
-print("Here are the colours: ",("\n",colours2,))
+    dig1=text[1-1]
+    dig2=text[2-1]
+    dig3=text[3-1]
+    dig4=text[4-1]
+    dig5=text[5-1]
 
- #the "dig_" are the seperate digits
-dig1=text[1-1]
-dig2=text[2-1]
- #printing the final product with colour
-print ("\n","Here is the final MOTD code:",colours['black'])
+    print("There are these many colours to choose from:",len(colours))
+    print("Here are the colours: \n black, dark blue, dark green, dark aqua, dark red, dark purple, gold, gray, dark gray, indigo, green, aqua, red, pink, yellow, white")
+
+    col1=input("please enter your first colour from the list: ")
+
+    print ("\n","Here is the final MOTD code:",col1,dig1)
+
+    restart=input("\nWould you like to repeat? [y/n] ")
